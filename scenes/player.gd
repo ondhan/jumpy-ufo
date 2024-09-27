70207eda5dce723d1ecf75021c8844b0bf33aa94
DMSMain <105548836+DMSMain@users.noreply.github.com>
2024-09-27 14:34:56 +0200

    Update 2 - player, obstacle
    - added player with animations and collisions
    - added obstacle with hitbox
    - added collision detection that hides the player
    - added custom keys (left, right, A, D, space, Enter) for moving and jumping

---
+++ scenes/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+
+
+func _on_obstacle_body_entered(body: Node2D) -> void:
+	self.hide()
+	pass # Replace with function body.
